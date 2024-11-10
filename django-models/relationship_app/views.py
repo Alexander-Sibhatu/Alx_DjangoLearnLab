5540be07d068f90b869d1ec8066debe825c6edf3
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-11-10 15:43:35 +0000

    reference edited as .models

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from relationship_app.models import Book
 
 from django.views.generic import DetailView
-from relationship_app.models import Library
+from .models import Library
 
 # Create your views here.
 def list_books(request):
