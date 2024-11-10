d24fe480ba77814e902b53f2c6a100b8178307e5
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-11-10 18:09:18 +0000

    views done

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from relationship_app.models import Book
-
 from django.views.generic.detail import DetailView
 from .models import Library
 
@@ -10,8 +12,28 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-#class-based view
+# Class-based view for Library Detail
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in after successful registration
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# Login View (Using Django's built-in LoginView)
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout View (Using Django's built-in LogoutView)
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
