462cd3849a3060fc2db1a805c98daebf0d323c06
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-11-10 15:17:35 +0000

    FBV implemented

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from relationship_app.models import Book
 
 # Create your views here.
+def list_books(request):
+    # Fetch all books from the database
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
