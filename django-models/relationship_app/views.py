26ab3af2537a765d5adf2db8ca5a5661cbfd030f
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-11-10 15:26:47 +0000

    FBV and CBV created

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from relationship_app.models import Book
 
+from django.views.generic import DetailView
+from relationship_app.models import Library
+
 # Create your views here.
 def list_books(request):
     # Fetch all books from the database
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+#class-based view
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
