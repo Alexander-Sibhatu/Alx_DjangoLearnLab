1e3f32158cd9fd307077c8f3ec65412da27ceed0
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-11 20:06:20 +0000

    rest_framework.authtoken configured and DRF settings updated

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,7 +39,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # Optional, for web browsing
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Require authentication by default
+    ],
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
