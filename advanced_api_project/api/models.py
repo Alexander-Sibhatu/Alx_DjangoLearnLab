81fd839e0b857ebf62ae4206e8545e394ac37acf
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-12 22:16:20 +0000

    bookserializer year validation implemented

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='book')
+
+    def __str__(self):
+        return f"{self.title} {self.publication_year}"
+
+from rest_framework import serializers
+
+class BookSerlializer(serializers.ModelSerializer):
+    def validate_publication_year(self, value):
+        import datetime
+        current_year = datetime.date.today().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+    
+    class Meta:
+        model = Book
+        fields = ["id", "title", "publication_year", "author"]
+
