6be0efbb7cce5b14978279063f51e92176bdb586
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-12 21:47:25 +0000

    default database added in settings.py

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -41,6 +41,14 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
