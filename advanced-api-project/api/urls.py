a2dbd489dc96bd4fdf0e95b08574c9b8bfe2a18b
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-13 23:47:20 +0000

    id holder removed

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,7 @@ urlpatters = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail')
     path('books/create/', CreateView.as_view(), name='create-book'),
-    path('books/<int:pk>/update', UpdateView.as_view(), name='update-book'),
-    path('books/<int:pk>/delete', DeleteView.as_view(), name='delete-book'),
+    path('books/update', UpdateView.as_view(), name='update-book'),
+    path('books/delete', DeleteView.as_view(), name='delete-book'),
 ]
 
