b813f735d60b3171c0f6247034a3b9de00f41c52
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-16 15:38:23 +0000

    views for handling login, logout and profile management implemented

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Reistration successful! You can now log in.')
+            return redirect('login')
+        else:
+            form = CustomUserCreationForm()
+        return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.email = request.POST['email']
+        user.save()
+        messages.success(request, 'Profile updated successfully!')
+        return redirect('profile')
+    return render(request, 'registration/profile.html')
 
 # Create your views here.
