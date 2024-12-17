07384fe542de68a678c695f963d46de865ff56f8
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-17 20:30:09 +0000

    register url added

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,5 +6,7 @@ urlspatters = [
     path('login/', auth_views.LoginView.as_view(template_name='templates/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='templates/logout.html'),
           name='logout'),
+    path('register/', auth_views.LogoutView.as_view(template_name='templates/register.html'),
+          name='register'),
     path('profile/', views.profile_view, name='profile')
 ]
