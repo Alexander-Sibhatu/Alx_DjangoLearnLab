52f67f88747b5536cadacd34a971cdb4d3d2fd6d
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-20 12:24:11 +0000

    comment model implemented

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+# Post model
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -12,4 +13,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+# Comment model
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
 
