c5869b194e9ac70f33e89506d87aca5157d08ca1
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-22 17:17:33 +0000

    bug fixed

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = CustomUser
@@ -18,9 +18,9 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
-            username=validated_data['username']
-            email=validated_data['email']
-            password=validated_data['password']
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
         )
         Token.objects.create(user=user)
         return user
