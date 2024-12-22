3b641edc28283a153156cf4523438f05b6582bee
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-22 13:56:03 +0000

    serializers implemented

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,3 +7,19 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password', 'email']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username']
+            email=validated_data['email']
+            password=validated_data['password']
+        )
+        Token.objects.create(user=user)
+        return user
+    
+
