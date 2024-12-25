d21649b3cebc8d26ec35127e93d62c3c75a69826
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-25 11:59:30 +0000

    functionality to filter posts by authors followed-by-user implemented

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
@@ -20,3 +20,18 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class FollowingPostsView(generics.ListAPIView):
+    permissions_class = [permissions.IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+         # Get the list of users the current user is following
+        following_users = self.request.user.following.all()
+        # Filter posts authored by those users and order them by creation date
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
