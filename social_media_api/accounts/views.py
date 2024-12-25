b8d92e39110ed628c790cb952518d33a99ce7d5d
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-12-25 11:49:26 +0000

    generic follow and unfollow views implemented

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, permissions
 from rest_framework.authtoken.models import Token
 from .models import CustomUser
 from .serializers import RegisterSerializer, UserSerializer
@@ -31,3 +31,29 @@ class UserProfileView(APIView):
         user = request.user
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        user_to_follow_id = kwargs.get('user_id')
+        user_to_foolow = CustomUser.objects.filter(id=user_to_follow_id)
+        if not user_to_foolow:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        request.user.following.add(user_to_foolow)
+        return Response({'message': f'You are now following {user_to_foolow.username}.'}, status=status.HTTP_200_OK)
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        user_to_unfollow_id = kwargs.get('user_id')
+        user_to_unfollow = CustomUser.objects.filter(id=user_to_unfollow_id).first()
+        if not user_to_unfollow:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        request.user.following.remove(user_to_unfollow)
+        return Response({'message': f'You have unfollowed {user_to_unfollow.username}.'}, status=status.HTTP_200_OK)
